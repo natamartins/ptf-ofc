3da462a45ca4d5389b9165e91b6bf4e7671d0e62
natamartins <codedev@DESKTOP-EONVM29>
2023-04-28 05:18:28 -0300

    add icons of lucies

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -2,25 +2,32 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 
+import { Github, Linkedin, Instagram, Twitter } from 'lucide-react'
+
 import Logo from '../../../public/Logo.svg'
 
 const Nav = () => {
     const hover = 'hover:text-violet-800'
     return (
         <header>
-            <nav className='max-w-prose m-auto flex items-center justify-between p-5 mb-20 sm:mb-20'>
+            <nav className='max-w-prose m-auto flex relative items-center justify-between p-5 mb-20 sm:mb-20'>
                 <Image src={Logo} alt="esté logo é uma letra n" />
                 <div className='flex items-center gap-5'>
                     <Link href='https://www.linkedin.com/in/nat%C3%A3martins/' target='__blank' className={hover}>
-                        Linkedin
+                        <Linkedin size={20} />
                     </Link>
                     <Link href='https://github.com/natamartins' target='__blank' className={hover}>
-                        Github
+                        <Github size={20} />
                     </Link>
                     <Link href='https://www.instagram.com/eusounathanmartins/?theme=dark'
                         target='__blank'
                         className={hover}>
-                        Instagram
+                        <Instagram size={20} />
+                    </Link>
+                    <Link href='https://twitter.com/NataCodedev'
+                        target='__blank'
+                        className={hover}>
+                        <Twitter size={20} />
                     </Link>
                 </div>
             </nav>
